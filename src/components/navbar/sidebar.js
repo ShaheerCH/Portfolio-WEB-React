5061537e3932c71ac552e66e020eec6c3abbfa1e
Muhammad Shaheer <shaheer.chry@gmail.com>
2023-01-07 02:55:48 +0500

    Done

--- src/components/navbar/sidebar.js
+++ src/components/navbar/sidebar.js
@@ -1,14 +1,26 @@
 import './sidebar.scss';
-import { RiLinkedinLine } from "react-icons/ri";
-import * as icons from "react-icons/fi";
+import { RiLinkedinLine } from 'react-icons/ri';
+import * as icons from 'react-icons/fi';
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <ul className="sidebar__menu">
-        <li className="sidebar__item"> <RiLinkedinLine size={25} /> </li>
-        <li className="sidebar__item"> <icons.FiGithub size={25} /></li>
-        <li className="sidebar__item"> <icons.FiTwitter size={25} /></li>
+        <li className="sidebar__item">
+          <a href="https://www.linkedin.com/in/shaheerch/">
+            <RiLinkedinLine size={25} />
+          </a>
+        </li>
+        <li className="sidebar__item">
+          <a href="http://github.com/ShaheerCH">
+            <icons.FiGithub size={25} />
+          </a>
+        </li>
+        <li className="sidebar__item">
+          <a href="https://twitter.com/Shaheer_choudry">
+            <icons.FiTwitter size={25} />
+          </a>
+        </li>
       </ul>
       <div class="left__line"></div>
     </div>
