7f9cdf47507c4348dd14ba3bc119448ce206f849
Muhammad Shaheer <shaheer.chry@gmail.com>
2023-01-07 03:34:25 +0500

    done

--- src/components/projects/list.js
+++ src/components/projects/list.js
@@ -2,48 +2,53 @@ const list = () => {
   return [
     {
       title: 'Project 1',
-      image: 'https://www.templatefreebies.com/wp-content/uploads/2021/10/2021-10-10_22-52-17.png',
+      image:
+        'https://www.templatefreebies.com/wp-content/uploads/2021/10/2021-10-10_22-52-17.png',
       description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.',
+        'The Crypto Exchange Wallet website was developed using frontend technologies such as HTML, CSS, and JavaScript to create the user interface and design. These technologies were used to create a user-friendly and easy-to-navigate website with a responsive design that works on desktop and mobile devices.',
       stack: 'HTML, CSS, JavaScript, React, Node, Express, MongoDB',
-      link: '',
-      },
+      link: 'https://github.com/ShaheerCH/Rails-Capstone',
+    },
+    {
+      title: 'Doctor Appointment',
+      image:
+        'https://149842033.v2.pressablecdn.com/wp-content/uploads/2019/02/free-doctor-website-templates-1.jpg',
+      description:
+        'Doctors Appointment website was developed using HTML, CSS, and JavaScript for the frontend. The frontend also utilized the Bootstrap CSS framework to ensure consistent styling across different devices. The website was designed with a simple and intuitive interface, allowing patients to easily schedule appointments with their healthcare providers.',
+      stack: 'HTML, CSS, JavaScript, React, Node, Express, MongoDB',
+      link: 'https://github.com/ShaheerCH/Vet_clinic',
+    },
   ];
 };
 
 const adlist = () => {
   return [
     {
-      title: 'Project 1',
+      title: 'Calculator',link: 'https://github.com/ShaheerCH/calculator',
       imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
+      description:
+        'Calculator website is a reliable and convenient tool for performing basic arithmetic calculations online. It has a user-friendly interface and is accessible from any device with an internet connection.',
     },
     {
-      title: 'Project 2',
+      title: 'Geo-Location',link: 'https://github.com/ShaheerCH/Capstone',
       imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
+      description:
+        'Geo Location website is a platform that allows users to find the geographical location of an IP address or domain name,the website was built using HTML, CSS, and JavaScript to create the user interface and design',
     },
     {
-      title: 'Project 3',
+      title: 'Weather App',link: 'https://github.com/ShaheerCH/weather',
       imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
+      description:
+        'Weather apps are the most popular way to instantly access forecasts, warnings and other useful weather information on our mobile phones',
     },
     {
-      title: 'Project 4',
+      title: 'Todo App',
       imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
+      link: 'https://github.com/ShaheerCH/todo-list',
+      description:
+        'The ToDo List Web App is a simple project developed using JavaScript, bootstrap, and HTML. This project is an interesting project. Here, the user can add the number of to-do work details and you can see the details stored in the list form.',
     },
-    {
-      title: 'Project 5',
-      imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
-    },
-    {
-      title: 'Project 5',
-      imageUrl: '',
-      description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptates, voluptatem quibusdam, quod, voluptatibus voluptas repudiandae quidem voluptatum quae quia natus. Quisquam, quae voluptate. Quae, voluptate. Quae, voluptate. Quae, voluptate.'
-    }
-  ]
-}
+  ];
+};
 
-export {list, adlist};
+export { list, adlist };
